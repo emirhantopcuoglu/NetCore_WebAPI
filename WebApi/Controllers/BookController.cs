7c0ddce79b2205bfe669aa64dead2c5e534cc8b0
emirhantopcuoglu <emirhantopcuoglu386@gmail.com>
2024-06-24 21:05:31 +0300

    Entity,Model and Dto

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.GetBooks;
+using WebApi.DBOperations;
+
+namespace WebApi.AddControllers
+{
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : Controller
+    {
+        private readonly BookStoreDbContext _context;
+        public BookController(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+        /*private static List<Book> BookList = new List<Book>(){
+            new Book {
+                Id = 1,
+                Title = "Kitap1",
+                GenreId = 1,
+                PageCount = 200,
+                PublishDate = new DateTime(2001,6,12)
+                },
+                new Book {
+                Id = 2,
+                Title = "Kitap2",
+                GenreId = 2,
+                PageCount = 400,
+                PublishDate = new DateTime(2005,4,11)
+                },
+                new Book {
+                Id = 3,
+                Title = "Kitap3",
+                GenreId = 2,
+                PageCount = 705,
+                PublishDate = new DateTime(2010,9,1)}
+        };*/
+
+        [HttpGet]
+        public IActionResult GetBooks()
+        {
+            GetBooksQuery query = new GetBooksQuery(_context);
+            var result = query.Handle();
+            return Ok(result);
+        }
+        // Route ile get isteği
+        [HttpGet("{id}")]
+        public Book GetById(int id)
+        {
+            var book = _context.Books.Where(book => book.Id == id).SingleOrDefault();
+            return book;
+        }
+
+        /*
+        // Query string ile get isteği
+        [HttpGet]
+        public Book Get([FromQuery] string id)
+        {
+            var book = BookList.Where(book => book.Id == Convert.ToInt32(id)).SingleOrDefault();
+            return book;
+        }*/
+        [HttpPost]
+        public IActionResult AddBook([FromBody] CreateBookModel newBook)
+        {
+            CreateBookCommand command = new CreateBookCommand(_context);
+            try
+            {
+                command.Model = newBook;
+                command.Handle();
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook)
+        {
+            var book = _context.Books.SingleOrDefault(x => x.Id == id);
+            if(book is null)
+                return BadRequest();
+            book.GenreId = updatedBook.GenreId != default ? updatedBook.GenreId : book.GenreId;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id)
+        {
+            var book = _context.Books.SingleOrDefault(x => x.Id == id);
+            if (book is null) return BadRequest();
+
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebApi/DBOperations/DataGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.DBOperations
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new BookStoreDbContext(serviceProvider.GetRequiredService<DbContextOptions<BookStoreDbContext>>()))
+            {
+                if(context.Books.Any()) {
+                    return;
+                }
+                context.Books.AddRange(
+                    new Book
+                    {
+                         //Id = 1,
+                         Title = "Kitap1",
+                         GenreId = 1,
+                         PageCount = 200,
+                         PublishDate = new DateTime(2001, 6, 12)
+                        },
+                    new Book
+                    {
+                        //Id = 2,
+                        Title = "Kitap2",
+                        GenreId = 2,
+                        PageCount = 400,
+                        PublishDate = new DateTime(2005, 4, 11)
+                    },
+                    new Book
+                    {
+                        //Id = 3,
+                        Title = "Kitap3",
+                        GenreId = 2,
+                        PageCount = 705,
+                        PublishDate = new DateTime(2010, 9, 1)
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
